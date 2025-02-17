f30f376367a600a3dbd377ddae4da6fc209555a4
roque <almeida.raphaelroque@gmail.com>
2025-02-17 17:33:33 -0300

    ajustando SQL ativacoes

--- sql/query_ANSI.sql
+++ sql/query_ANSI.sql
@@ -22,23 +22,30 @@ from silver.TITULO T
 	INNER JOIN silver.TITULO_MOVIMENTO TM ON TM.PONTEIRO = T.PONTEIRO
 	AND TM.HISTORICO =1
 	AND (TM.PONTEIRO_CONSOLIDADO IS NULL OR TM.PONTEIRO_CONSOLIDADO= 0)
+
 	INNER JOIN silver.CATALOGO CAT ON CAT.PESSOA = T.PESSOA
 	AND CAT.CNPJ_CPF = T.CNPJ_CPF
+
 	INNER JOIN silver.APLICACAO_RECURSO_FINANCEIRO A ON T.CODIGO_APLICACAO_RECURSO_FIN = A.CODIGO
 	AND T.CODIGO_EMPRESA = A.CODIGO_EMPRESA
+
 	INNER JOIN silver.GRUPO_APLIC_REC_FINANCEIRO G ON A.CODIGO_GRUPO = G.CODIGO
 	AND G.CODIGO_EMPRESA = A.CODIGO_EMPRESA
+
 	LEFT OUTER JOIN silver.INVOICE_ITEM I ON TM.ID_TITULO_MOVIMENTO = I.ID_TITLE_MOVIMENT
 	LEFT OUTER JOIN silver.INVOICE IFF ON I.PARENT = IFF.ID
 	LEFT OUTER JOIN silver.INSURANCE_REG_SET IR ON IR.ID = IFF.ID_SET
 	LEFT OUTER JOIN silver.VENDEDOR V ON V.CODIGO = IR.ID_CONSULTANT
 	LEFT JOIN silver.representante r ON r.codigo = iff.id_unity
 	LEFT JOIN silver.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+	LEFT JOIN insurance_status ins ON IR.id_status = ins.id
+	
 
 
 
 where date_diff('day',cast(cast(T.DATA_VENCIMENTO as timestamp) as date),current_date) > 0
 and t.CRC_CPG = 'R'
+and ins.id = 7
 
 
 ------------------------------------------------------------------------
@@ -83,10 +90,12 @@ from stcoop.TITULO T
 	LEFT OUTER JOIN stcoop.VENDEDOR V ON V.CODIGO = IR.ID_CONSULTANT
 	LEFT JOIN stcoop.representante r ON r.codigo = iff.id_unity
 	LEFT JOIN stcoop.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+	LEFT JOIN insurance_status ins ON IR.id_status = ins.id
 
 
 where date_diff('day',cast(cast(T.DATA_VENCIMENTO as timestamp) as date),current_date) > 0
 and t.CRC_CPG = 'R'
+and ins.id = 7
 
 
 ------------------------------------------------------------------------
@@ -132,8 +141,10 @@ from viavante.TITULO T
 	LEFT OUTER JOIN viavante.VENDEDOR V ON V.CODIGO = IR.ID_CONSULTANT
 	LEFT JOIN viavante.representante r ON r.codigo = iff.id_unity
 	LEFT JOIN viavante.catalogo cata ON cata.cnpj_cpf = r.cnpj_cpf
+	LEFT JOIN insurance_status ins ON IR.id_status = ins.id
 
 
 where date_diff('day',cast(cast(T.DATA_VENCIMENTO as timestamp) as date),current_date) > 0
 and t.CRC_CPG = 'R'
+and ins.id = 7
 
